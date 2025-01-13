beb02a80ed1e1f49ea43203a2d237542ffb54e04
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-01-13 09:12:17 +0100

    exercici 2

--- ReviewExercises/MainProgram/Program.cs
+++ ReviewExercises/MainProgram/Program.cs
@@ -7,7 +7,13 @@ namespace MainProgram
         {
             const string MsgBase = "La base del triangle: ";
             const string MsgAlt = "La altura del triangle: ";
-            
+            float baseTriangle, alturaTriangle, areaTriangle;
+            Console.Write(MsgBase);
+            baseTriangle = float.Parse(Console.ReadLine());
+            Console.Write(MsgAlt);
+            alturaTriangle = float.Parse(Console.ReadLine());
+            areaTriangle = baseTriangle * alturaTriangle / 2;
+            Console.Write(areaTriangle);
         }
     }
 }
